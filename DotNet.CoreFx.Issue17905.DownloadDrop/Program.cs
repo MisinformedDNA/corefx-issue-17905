503a8fe3e337a5cbc5adcaa84bde5a82dff8eb09
Dan Friedman <dlfriedman@landolakes.com>
2018-03-28 12:17:07 -0500

    Add project files.

---
+++ DotNet.CoreFx.Issue17905.DownloadDrop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNet.CoreFx.Issue17905.DownloadDrop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
